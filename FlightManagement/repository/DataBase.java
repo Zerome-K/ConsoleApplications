64d152f0a57337b66f64c3cd1525b8a2a08b5a55
Zerome <110775338+Zerome-K@users.noreply.github.com>
2022-11-10 22:31:05 +0530

    Add files via upload

---
+++ FlightManagement/repository/DataBase.java
@@ -0,0 +1,48 @@
+package flightmanagementsystem.repository;
+
+import flightmanagementsystem.model.Flight;
+import flightmanagementsystem.model.Passenger;
+import flightmanagementsystem.model.Ticket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataBase {
+    private static DataBase dataBase = null;
+
+    private DataBase() {}
+
+    public static DataBase getInstance(){
+        if (dataBase != null)
+            dataBase = new DataBase();
+        return dataBase;
+    }
+
+    private List<Flight> flights = new ArrayList<>();
+    private List<Passenger> passengers = new ArrayList<>();
+    private List<Ticket> tickets = new ArrayList<>();
+
+    public List<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(List<Passenger> passengers) {
+        this.passengers = passengers;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public List<Flight> getFlights() {
+        return this.flights;
+    }
+
+    public void setFlights(List<Flight> flights) {
+        this.flights = flights;
+    }
+}
