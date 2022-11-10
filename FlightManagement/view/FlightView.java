a9d657fe953e6870eb380f9dbbfdd8aca67ab752
Zerome <110775338+Zerome-K@users.noreply.github.com>
2022-11-10 23:15:41 +0530

    Update FlightView.java

--- FlightManagement/view/FlightView.java
+++ FlightManagement/view/FlightView.java
@@ -80,9 +80,4 @@ public class FlightView {
     public void displayMessage(String message) {
         System.out.println(message);
     }
-
-    public static void main(String[] args) {
-        FlightView fv = new FlightView();
-
-    }
 }
