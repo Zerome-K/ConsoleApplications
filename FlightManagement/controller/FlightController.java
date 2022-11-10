2b8aa7fcebb4a983ac2ca0980e138bf994e192ce
Zerome <110775338+Zerome-K@users.noreply.github.com>
2022-11-10 22:29:36 +0530

    Add files via upload

---
+++ FlightManagement/controller/FlightController.java
@@ -0,0 +1,30 @@
+package flightmanagementsystem.controller;
+
+import flightmanagementsystem.model.Flight;
+import flightmanagementsystem.repository.DataBase;
+import flightmanagementsystem.view.FlightView;
+
+import java.util.List;
+
+public class FlightController {
+    private final FlightView flightView;
+
+    public FlightController(FlightView flightView) {
+        this.flightView = flightView;
+    }
+    public void addFlight(Flight flight) {
+        List<Flight> flights = DataBase.getInstance().getFlights();
+        flights.add(flight);
+        DataBase.getInstance().setFlights(flights);
+        String message = "FLIGHT ADDED SUCCESSFULLY";
+        flightView.displayMessage(message);
+    }
+    public void listRoutes(int number) {
+        for (Flight flight : DataBase.getInstance().getFlights()) {
+            if (flight.getFlightNumber() == number) {
+                flightView.displayMessage(flight.getVia().toString());
+                break;
+            }
+        }
+    }
+}
---
+++ FlightManagement/controller/TicketController.java
@@ -0,0 +1,43 @@
+package flightmanagementsystem.controller;
+
+import flightmanagementsystem.model.City;
+import flightmanagementsystem.model.Flight;
+import flightmanagementsystem.repository.DataBase;
+import flightmanagementsystem.view.TicketView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+public class TicketController {
+    private final TicketView ticketView;
+
+    public TicketController(TicketView ticketView) {
+        this.ticketView = ticketView;
+    }
+
+    public void checkFlights(String fromStation, String toStation) {
+        List<Flight> flights = DataBase.getInstance().getFlights();
+        HashMap<Flight, Integer> availableFlight = new HashMap<>();
+        for (Flight flight : flights) {
+            List<City> via = flight.getVia();
+            boolean flag = false;
+            int km = 0, start = 0;
+            for (City city : via) {
+                if (Objects.equals(city.getCityName(), fromStation)) flag = true;
+                else if (flag) {
+                    km += city.getMilesFromDestination();
+                    if (Objects.equals(city.getCityName(), toStation)) flag = false;
+                }
+            }
+            availableFlight.put(flight,km);
+        }
+    }
+
+    public void bookFlight(int number) {
+        List<Flight> flights = DataBase.getInstance().getFlights();
+        for (Flight flight:flights) {
+            if(flight.getFlightNumber() == number)
+        }
+    }
+}
